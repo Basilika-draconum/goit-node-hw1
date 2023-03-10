39ef9b4242a9392325ec52893d83acf96df94452
Lika <14dubinenkoanzhelika@gmail.com>
2023-03-10 17:58:29 +0200

    init files

---
+++ contacts.js
@@ -0,0 +1,23 @@
+import fs from "fs";
+import path from "path";
+
+// const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const contactsPath = path.join("/", "db", "contacts.json");
+console.log(contactsPath);
+
+// TODO: задокументувати кожну функцію
+function listContacts() {
+  // ...твій код
+}
+
+function getContactById(contactId) {
+  // ...твій код
+}
+
+function removeContact(contactId) {
+  // ...твій код
+}
+
+function addContact(name, email, phone) {
+  // ...твій код
+}
